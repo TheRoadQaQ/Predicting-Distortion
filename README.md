19dc5adc257438d0070fa3008ac41faeec97c764
TheRoadQaQ <maluqaq@163.com>
2025-03-31 19:07:54 +0800

    dataset

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Evaluating and Predicting Distorted Human Body Parts for Generated Images
-The model and dataset will be made publicly available soon.
+HF Dataset: RoadQAQ/Distorted-5K
+The model weight will be made publicly available soon.
--- ViT-HD/qwen2_vl_encoder_pixel_classification.py
+++ ViT-HD/qwen2_vl_encoder_pixel_classification.py
@@ -6,20 +6,15 @@ import torch
 import torch.nn as nn
 
 class Qwen2VLTForMalformPixelDetection(nn.Module):
-    def __init__(self, num_classes, model_path,  use_focal_loss = False, focal_alpha = 1.0, focal_gamma = 0.0):
+    def __init__(self, num_classes, model_path):
         super(Qwen2VLTForMalformPixelDetection, self).__init__()
 
         config = Qwen2VLVisionConfig.from_pretrained(model_path)
         self.vision_encoder = Qwen2VLVisionEncoder(config)
         #state_dict = load_file('./qwen-vl-encoder.safetensors')
         #self.vision_encoder.load_state_dict(state_dict)
-        print(self.vision_encoder)
         self.num_classes = num_classes
         self.patch_size = 14
-
-        self.use_focal_loss = use_focal_loss
-        self.focal_alpha = focal_alpha
-        self.focal_gamma = focal_gamma
 
         # Define an MLP for classification
         # h = 1280
@@ -31,10 +26,6 @@ class Qwen2VLTForMalformPixelDetection(nn.Module):
             nn.Linear(256, num_classes)
         )
 
-    # num_patch = w//p * h//p = 32*32 = 1024
-    # images: (b, w//p * h//p, h')
-    # grid_thws: (b, 3)
-    # labels: (b, resolution, resolution)
     def forward(self, images, grid_thws, pixel_labels = None):
        
         batch_size = images.shape[0]
@@ -42,12 +33,11 @@ class Qwen2VLTForMalformPixelDetection(nn.Module):
         # (b, w//p * h//p, h')  ->  [(b * num_patch, h) x 32]
         _, hidden_states_list = self.vision_encoder(images, grid_thws) 
 
-        hidden_state_8 = hidden_states_list[7]   # 索引从0开始，第8层对应索引7
-        hidden_state_16 = hidden_states_list[15] # 第16层对应索引15
-        hidden_state_24 = hidden_states_list[23] # 第24层对应索引23
-        hidden_state_32 = hidden_states_list[31] # 第32层对应索引31
+        hidden_state_8 = hidden_states_list[7]   
+        hidden_state_16 = hidden_states_list[15] 
+        hidden_state_24 = hidden_states_list[23] 
+        hidden_state_32 = hidden_states_list[31] 
 
-        # 在最后一个维度上进行拼接  (b * num_patch, 4h)
         outputs = torch.cat((hidden_state_8, hidden_state_16, hidden_state_24, hidden_state_32), dim=-1)
 
         # Classification
@@ -68,12 +58,7 @@ class Qwen2VLTForMalformPixelDetection(nn.Module):
         if pixel_labels is not None:
             # pixel_labels: (b, resolution, resolution,) 
             pixel_labels = pixel_labels.view(-1)
-            #loss = F.cross_entropy(logits, pixel_labels)
-
-            if self.use_focal_loss:
-                loss = focal_loss(logits, pixel_labels, self.focal_gamma, self.focal_alpha)
-            else:
-                loss = F.cross_entropy(logits, pixel_labels)
+            loss = F.cross_entropy(logits, pixel_labels)
 
         return ImageClassifierOutput(
             loss=loss,
